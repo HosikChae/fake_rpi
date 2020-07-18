46dd5101535e08b4211bc255bc5bec7220e3bbdd
Hosik Chae <CKMagenta@gmail.com>
2020-07-18 19:01:36 +0900

    set any non-trivial value to os.environ['FAKE_RPI-SUPRESS_MSG'] to supress the welcoming message

--- fake_rpi/Base.py
+++ fake_rpi/Base.py
@@ -3,9 +3,11 @@
 # Copyright (c) 2017 Kevin Walchko
 # see LICENSE for full details
 ##############################################
+import os
 
 class Base(object):
     def __init__(self, name=None):
-        print('<<< WARNING: using fake raspberry pi interfaces >>>')
-        if name:
-            print('<<< Using: {} >>>'.format(name))
+        if bool(os.environ.get('FAKE_RPI_SUPRESS_MSG', '')) == False:
+            print('<<< WARNING: using fake raspberry pi interfaces >>>')
+            if name:
+                print('<<< Using: {} >>>'.format(name))
